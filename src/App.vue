7a097b94b23335662183d51b40b57df5e546b481
chesteric31 <eric.binard.182@gmail.com>
2023-02-11 22:26:38 +0100

    add primevue

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,20 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <!--<a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>-->
+    <a href="">
+      <img src="./assets/pegasus.png" class="logo pegasus" alt="Pegasus logo"/>
     </a>
+    <p>
+      <Button>Boum</Button>
+    </p>
   </div>
-  <HelloWorld msg="Welcome on Pegasus!" />
+  <HelloWorld msg="Welcome on Pegasus!"/>
 </template>
 
 <style scoped>
@@ -21,9 +27,15 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.pegasus {
+  height: auto;
 }
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em darkgoldenrod);
+}
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).component('Button', Button).mount('#app')
